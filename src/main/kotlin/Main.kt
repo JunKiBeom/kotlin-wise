e243d7f868b1adaab108e1a1dfab3b1b53c8ab03
JunKiBeom <dona0408@gmail.com>
2025-03-20 15:44:51 +0900

    5단계 - 목록 구현

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,8 @@
+import domain.wiseSaying.entity.WiseSaying
+
 fun main() {
+
+    val wiseSayings = mutableListOf<WiseSaying>()
 
     var lastId = 0
 
@@ -14,8 +18,18 @@ fun main() {
                 val saying = readlnOrNull() ?: ""
                 print("작가: ")
                 val author = readlnOrNull() ?: ""
+                val id = ++lastId
+                wiseSayings.add(WiseSaying(id, saying, author))
 
-                println("${++lastId}번 명언이 등록되었습니다.")
+                println("${lastId}번 명언이 등록되었습니다.")
+            }
+
+            "목록" -> {
+                println("번호 / 작가 / 명언")
+                println("----------------------")
+                wiseSayings.forEach {
+                    println("${it.id} / ${it.author} / ${it.saying}")
+                }
             }
         }
     }
