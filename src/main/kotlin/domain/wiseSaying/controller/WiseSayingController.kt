da11fd55eb83ef5c06176da742b46faecfe45e17
JunKiBeom <dona0408@gmail.com>
2025-03-25 02:34:49 +0900

    singleton apply

--- src/main/kotlin/domain/wiseSaying/controller/WiseSayingController.kt
+++ src/main/kotlin/domain/wiseSaying/controller/WiseSayingController.kt
@@ -2,9 +2,10 @@ package domain.wiseSaying.controller
 
 import domain.wiseSaying.service.WiseSayingService
 import global.Request
+import global.SingletonScope
 
 class WiseSayingController{
-    private val wiseSayingService = WiseSayingService()
+    private val wiseSayingService = SingletonScope.wiseSayingService
 
     fun write() {
         print("명언: ")
