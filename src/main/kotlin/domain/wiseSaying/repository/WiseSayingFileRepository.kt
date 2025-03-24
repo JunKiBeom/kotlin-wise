885081e47797506066f80b6766de71c41de77c88
JunKiBeom <dona0408@gmail.com>
2025-03-25 02:55:04 +0900

    saveOnDisk & save test case

--- src/main/kotlin/domain/wiseSaying/repository/WiseSayingFileRepository.kt
+++ src/main/kotlin/domain/wiseSaying/repository/WiseSayingFileRepository.kt
@@ -1,10 +1,25 @@
 package domain.wiseSaying.repository
 
 import domain.wiseSaying.entity.WiseSaying
+import java.nio.file.Path
 
 class WiseSayingFileRepository : WiseSayingRepository {
+
+    private var lastId: Int = 0
+
     override fun save(wiseSaying: WiseSaying): WiseSaying {
-        TODO("Not yet implemented")
+        if (wiseSaying.isNew()) {
+            val new = wiseSaying.copy(id = ++lastId)
+            saveOnDisk(new)
+            return new
+        }
+
+        saveOnDisk(wiseSaying)
+        return wiseSaying
+    }
+
+    fun saveOnDisk(wiseSaying: WiseSaying) {
+        Path.of("data/dev/wiseSaying").toFile().writeText(wiseSaying.jsonStr)
     }
 
     override fun findAll(): List<WiseSaying> {
